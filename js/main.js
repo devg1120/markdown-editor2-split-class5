c211ab692f116b2126af9f2cf14c2dcb00fe9cd6
devg1120 <devg1120@gmail.com>
2024-05-11 17:58:22 +0900

    first

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+import { MarkDownPanel } from "./MarkDownPanel.js";
+/*
+let base1 = document.querySelector("#markdown-editor1");
+let mp1 = new MarkDownPanel(base1);
+mp1.init();
+
+let base2 = document.querySelector("#markdown-editor2");
+let mp2 = new MarkDownPanel(base2);
+mp2.init();
+*/
+let mp1 = new MarkDownPanel("#markdown-editor1");
+mp1.init();
+
+let mp2 = new MarkDownPanel("#markdown-editor2");
+mp2.init();
+
+
